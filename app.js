81cea5f413069c023cf680bbae7d84ee94d2dc25
半田裕紀 <handayuuki@handayuukis-MacBook-Air.local>
2022-01-30 15:42:53 +0900

    script初歩

---
+++ app.js
@@ -0,0 +1,45 @@
+
+let x = 100;
+let y = 50;
+
+let z = x * y;
+let m = x - y;
+let w = x / y;
+let p = x + y;
+
+
+
+alert(
+  x + '/' + y + '=' + w + 'です'
+  );
+
+// // let orange = 100;
+// // let apple = 120;
+
+// // if (orange < apple){
+// //   alert('ミカンの値段がリンゴより安い');
+// // }else if (orange == apple){
+// //   alert('みかんとりんごが同じ値段');
+// // }else{
+// //   alert('みかんの値段がリンゴより高い');
+// // }
+
+// // let max = 100;
+// // let num = 1;
+// // let count = 0;
+
+// // while(num < max){
+// //   num = num * 2;
+// //   count = count + 1;
+// // }
+
+// // alert('２を掛け続けて'+ max + 'を超えるのに必要だった回数は' + count +'回です');
+
+// let i;
+// let num = 0;
+
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足した結果は' + num + 'です');
