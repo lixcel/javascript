1eadac18c86e0f43decc088bd9e406cb2148db49
半田裕紀 <handayuuki@handayuukis-MacBook-Air.local>
2022-02-01 18:16:15 +0900

    [finish]

--- practice-variable/sample.js
+++ practice-variable/sample.js
@@ -1,9 +1,24 @@
-let alertString;
-alertString = addString("WebCamp");
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーからから選んでください。');
 
-alert(alertString);
+let js_hand = getJShand()
 
-function addString(strA){
-  let addStr = "Hello" + strA;
-  return addStr;
+let judge = winLose(user_hand, js_hand)
+
+alert('あなたの選んだ手は' + user_hand + 'です。¥nJavaScriptの選んだ手は' + )
+
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3);
+
+
+  if(js_hand_num == 0){
+    js_hand = "グー"
+  } else if(js_hand_num == 1){
+    js_hand = "チョキ"
+  } else if(js_hand_num == 2){
+    js_hand = "パー"
+  }
+
+  return js_hand;
 }
+
+alert('あなたの選んだ手は' + user_hand + 'です。')
